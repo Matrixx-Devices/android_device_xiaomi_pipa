d58ae4ffe1154345e853f9b05a100632f4416687
CuriousNom <aryanbiswas7585@gmail.com>
2025-02-07 23:21:01 +0530

    pipa: Update branch for memecam

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone --depth 1 https://github.com/CuriousNom/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
-git clone --depth 1 https://github.com/CuriousNom/proprietary_vendor_xiaomi_pipa vendor/xiaomi/pipa 
+git clone --depth 1 https://github.com/CuriousNom/proprietary_vendor_xiaomi_pipa -b test vendor/xiaomi/pipa 
 git clone --depth 1 https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
 git clone --depth 1 https://github.com/CuriousNom/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250
 git clone --depth 1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
